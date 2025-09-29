c93984d836c1e2711b9508a18dd9dd591f4f505e
Subathra <sduria@mgconsultingfirm.com>
2025-09-29 20:52:55 +0530

    revert the previous commit

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,4 +1,3 @@
-import video from "https://www.choolsgroup.com/iffai/IFFAI%20AI%20ALL.mp4";
 const Hero = () => {
   return (
     <section className="relative w-screen h-screen overflow-hidden">
@@ -9,7 +8,7 @@ const Hero = () => {
         loop
         playsInline
       >
-        <source src={video} type="video/mp4" />
+        <source src="/src/assets/iffai video final.mp4" type="video/mp4" />
         Your browser does not support the video tag.
       </video>
 
